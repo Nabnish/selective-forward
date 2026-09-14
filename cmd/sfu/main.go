ae3c6408621612c5d9c95a8e84aca13b376e0454
Nabeel <nabeelpasha74@gmail.com>
2026-09-14 14:22:30 +0530

    ice connection established with sfu

--- cmd/sfu/main.go
+++ cmd/sfu/main.go
@@ -11,16 +11,75 @@ import (
 )
 
 type msg struct{
-	Event string : `json."event"`
-	data json.RawMessage : `json."data"`
+	Event string  `json."event"`
+	Data json.RawMessage  `json."data"`
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {return true},
 }
 
 func main(){
-	http.HandleFunc("/",helloHandler)
-	fmt.println("server started")
-	http.ListenAndAnswer(":8080",nil)
+	http.Handle("/", http.FileServer(http.Dir("web/public")))
+	http.HandleFunc("/ws",handleWS)
+	fmt.Println("server started")
+	http.ListenAndServe(":8080",nil)
 }
+
+func handleWS(W http.ResponseWriter,r *http.Request){
+	conn , err := upgrader.Upgrade(W,r,nil)
+	if(err!=nil){
+		log.Println("upgrade:",err)
+		return
+	}
+	defer conn.Close()
+	
+	pc , err :=webrtc.NewPeerConnection(webrtc.Configuration{
+		ICEServers : []webrtc.ICEServer{{URLs  : []string{"stun:stun.l.google.com:19302"}}},
+	})
+
+	if err!=nil{
+		log.Println("connection failed",err)
+		return 
+	}
+	defer pc.Close()
+
+	pc.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		log.Println("connection state:", s)
+	})
+
+	for{
+		var message msg
+		if err := conn.ReadJSON(&message); err!=nil{
+			log.Println("error",err)
+			return
+		}
+		log.Println("got event",message.Event)
+		if message.Event == "offer"{
+			var offer webrtc.SessionDescription
+			if err := json.Unmarshal(message.Data,&offer); err!=nil{
+				log.Println("bad offer:", err)
+				continue
+			}
+			if err := pc.SetRemoteDescription(offer); err != nil {
+				log.Println("SetRemoteDescription:", err)
+				continue
+			}
+			answer, err := pc.CreateAnswer(nil)
+			if err != nil {
+				log.Println("CreateAnswer:", err)
+				continue
+			}
+			if err := pc.SetLocalDescription(answer); err != nil {
+				log.Println("SetLocalDescription:", err)
+				continue
+			}
+			b, _ := json.Marshal(pc.LocalDescription())
+			conn.WriteJSON(msg{Event: "answer", Data: b})
+			log.Println("sent answer")
+		}
+	}
+}
+	
+
+
